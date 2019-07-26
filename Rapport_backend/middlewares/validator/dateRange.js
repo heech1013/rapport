0715f635c488c28efd13b401b8cd3043b37fdcbe
aiden shin <heech1013@gmail.com>
2019-07-26 13:12:57 +0900

    Update: reserve condition

--- Rapport_backend/middlewares/validator/dateRange.js
+++ Rapport_backend/middlewares/validator/dateRange.js
@@ -9,13 +9,14 @@ const dateRangeValidator = (str, date1, date2) => {
   return new Promise((resolve, reject) => {
     const dateRangeErr = CustomError('BadRequest', 'Date is out of range.');
 
-    /* 상담사 검색 / 상담 예약: 당일 다음 날부터 13주 후 토요일까지 가능(일요일 - 토요일을 일주일 기준으로 한다) */
+    /* 상담사 검색 / 상담 예약: 당일로부터 이틀 후 부터 13주 후 토요일까지 가능(일요일 - 토요일을 일주일 기준으로 한다) */
     if (str === 'reservation') {
       const dayNum = new Date().getDay();
       const today = format(new Date(), 'YYYY-MM-DD');
+      const tommorow = format( addDays(today, 1), 'YYYY-MM-DD' );
       const maxDate = format( addDays(today, 91 + (6 - dayNum)), 'YYYY-MM-DD' );
       if (isAfter(date1, maxDate)) reject( dateRangeErr );
-      else if (!isAfter(date1, today)) reject( dateRangeErr );
+      else if (!isAfter(date1, tommorow)) reject( dateRangeErr );
     }
     /* 예약 취소: 오늘 (포함) 이후, 오늘 이전 날짜는 불가능 */
     else if (str === 'future') {
