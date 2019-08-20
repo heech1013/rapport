92a1cf44d50d9f976b9fa2c29efa4cac75345999
aiden shin <heech1013@gmail.com>
2019-08-20 21:27:47 +0900

    Processing: multipart/form-data update

--- Rapport_backend/routes/counselor/profile/update.js
+++ Rapport_backend/routes/counselor/profile/update.js
@@ -30,23 +30,25 @@ const update = async (req, res, next) => {
 
     /* 업로드한 파일을 임시 경로에 저장한다. */
     form.parse(req, (err, fields, files) => {
-      const params = {
-        Bucket: 'rapport-img',  // S3 bucket 설정
-        Key: `${newUser.id}/프로필`, // S3에 저장될 파일 이름 설정
-        ACL: 'public-read',  // 접근 권한. public-read여야 웹에서 이미지로 접근 가능
-        Body: fs.createReadStream(files.profileImg.path),  // files(업로드 파일 정보).input_file(<form>의 <input>의 지정한 name 명).path(해당 파일이 저장된 임시 경로)
-        ContentType: 'image/jpg'  // 웹에서 이미지를 로드했을 때 자동으로 파일이 다운로드 되는 것을 방지한다.
-      };
-
+      /* 프로필 사진 업데이트 */
       /* 특정 파일을 제출했을 때만 S3에 업데이트한다. (S3의 파일명으로 지정되는 Key 값이 같으므로 기존 이미지가 새로 대체된다) */
       if (files.profileImg.size) {
-        S3.upload(params, (err, data) => {
+        const paramsForProfile = {
+          Bucket: 'rapport-img',  // S3 bucket 설정
+          Key: `${newUser.id}/프로필`, // S3에 저장될 파일 이름 설정
+          ACL: 'public-read',  // 접근 권한. public-read여야 웹에서 이미지로 접근 가능
+          Body: fs.createReadStream(files.profileImg.path),  // files(업로드 파일 정보).input_file(<form>의 <input>의 지정한 name 명).path(해당 파일이 저장된 임시 경로)
+          ContentType: 'image/jpg'  // 웹에서 이미지를 로드했을 때 자동으로 파일이 다운로드 되는 것을 방지한다.
+        }
+        /* 임시경로에 저장된 프로필 파일을 S3에 업로드한다. */
+        // size가 0이어도(파일을 제출하지 않아도) s3에 저장이 되며 src가 할당된다.
+        S3.upload(paramsForProfile, (err, data) => {
           if (err) {
             return next(err);
           } else {
-            console.log('S3 Upload Success. img_src: ', data.Location);
+            console.log('profileImg S3 Upload Success. img_src: ', data.Location);
             /* 업로드한 프로필 사진의 src를 해당 유저의 DB에 업데이트한다. */
-            CounselorProfile.update({ profileImgSrc: data.Location }, { where: { fkCounselorId: id }});
+            CounselorProfile.update({ profileImgSrc: data.Location }, { where: { fkCounselorId: newUser.id }});
           }
         });
       }
@@ -56,6 +58,60 @@ const update = async (req, res, next) => {
           next(err);
         } else {
           console.log('Temp Files Delete Success. temparary file path: ', files.profileImg.path);
+        }
+      });
+
+      /* 자격증(상담심리사) 사진 업데이트 */
+      if (files.KCounselingPA.size) {
+        const paramsForKCounselingPA = {
+          Bucket: 'rapport-img',  // S3 bucket 설정
+          Key: `${newUser.id}/상담심리사`, // S3에 저장될 파일 이름 설정
+          ACL: 'public-read',  // 접근 권한. public-read여야 웹에서 이미지로 접근 가능
+          Body: fs.createReadStream(files.KCounselingPAImg.path),  // files(업로드 파일 정보).input_file(<form>의 <input>의 지정한 name 명).path(해당 파일이 저장된 임시 경로)
+          ContentType: 'image/jpg'  // 웹에서 이미지를 로드했을 때 자동으로 파일이 다운로드 되는 것을 방지한다.
+        }
+        S3.upload(paramsForKCounselingPA, (err, data) => {
+          if (err) {
+            return next(err);
+          } else {
+            console.log('KCounselingPA S3 Upload Success. img_src: ', data.Location);
+            /* 업로드한 프로필 사진의 src를 해당 유저의 DB에 업데이트한다. */
+            Certification.update({ KCounselingPAImgSrc: data.Location }, { where: { fkCounselorId: newUser.id }});
+          }
+        });
+      }
+      fs.unlink(files.KCounselingPAImg.path, (err) => {
+        if (err) {
+          next(err);
+        } else {
+          console.log('Temp Files Delete Success. temparary file path: ', files.KCounselingPAImg.path);
+        }
+      });
+
+      /* 자격증(임상심리전문가) 사진 업데이트 */
+      if (files.KClinicalPA.size) {
+        const paramsForKClinicalPA = {
+          Bucket: 'rapport-img',  // S3 bucket 설정
+          Key: `${newUser.id}/상담심리사`, // S3에 저장될 파일 이름 설정
+          ACL: 'public-read',  // 접근 권한. public-read여야 웹에서 이미지로 접근 가능
+          Body: fs.createReadStream(files.KClinicalPAImg.path),  // files(업로드 파일 정보).input_file(<form>의 <input>의 지정한 name 명).path(해당 파일이 저장된 임시 경로)
+          ContentType: 'image/jpg'  // 웹에서 이미지를 로드했을 때 자동으로 파일이 다운로드 되는 것을 방지한다.
+        }
+        S3.upload(paramsForKClinicalPA, (err, data) => {
+          if (err) {
+            return next(err);
+          } else {
+            console.log('KClinicalPA S3 Upload Success. img_src: ', data.Location);
+            /* 업로드한 프로필 사진의 src를 해당 유저의 DB에 업데이트한다. */
+            Certification.update({ KClinicalPAImgSrc: data.Location }, { where: { fkCounselorId: newUser.id }});
+          }
+        });
+      }
+      fs.unlink(files.KClinicalPAImg.path, (err) => {
+        if (err) {
+          next(err);
+        } else {
+          console.log('Temp Files Delete Success. temparary file path: ', files.KClinicalPAImg.path);
         }
       });
     });
--- Rapport_backend/routes/s3Form.js
+++ Rapport_backend/routes/s3Form.js
@@ -3,6 +3,7 @@ const s3Form = (req, res) => {
     <html>
     <body>
       <form enctype='multipart/form-data' method='post' action='http://localhost:5959/s3'>
+        <input type='text' name='profileText'>
         <input type='file' name='profileImg' accept='image/*'>
         <input type='submit'>
       </form>
