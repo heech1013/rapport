ffcdf2ac53ae95931c275f820c46c5d4a9f89263
Aiden Shin <heech1013@gmail.com>
2021-09-06 21:55:21 +0900

    refactoring: transporterFunc.js

--- middlewares/mailer/transporterFunc.js
+++ middlewares/mailer/transporterFunc.js
@@ -2,18 +2,12 @@ const nodemailer = require('nodemailer');
 
 require('dotenv').config();
 
-const transporter = () => {
-  return new Promise((resolve) => {
-    resolve(
-      nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'rapport5959@gmail.com',
-          pass: process.env.EMAIL_PASSWORD
-        }
-      })
-    )
-  })
-}
+const transporter = () => nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'rapport5959@gmail.com',
+    pass: process.env.EMAIL_PASSWORD
+  }
+})
 
 module.exports = transporter;
