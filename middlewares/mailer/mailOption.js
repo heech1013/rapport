e0219ea79316409f5c2e057d9c78c6d0dc90b1ef
Aiden Shin <heech1013@gmail.com>
2021-09-06 21:57:21 +0900

    refactoring: mailOption.js

--- middlewares/mailer/mailOption.js
+++ middlewares/mailer/mailOption.js
@@ -1,25 +1,15 @@
 require('dotenv').config();
 
-const CustomError = require('../errorHandler/customError');
-
 const mailOption = (type) => {
-  return new Promise( async(resolve, reject) => {
-    try {
-      const mailOption = {
-        from: "'라포 알림 서버' rapport5959@gmail.com",
-        to: 'rapport5959@naver.com',
-        subject: null,
-        html: '<p>라포 알림 서버에서 발송된 메일입니다.<p>'
-      }
-      if (type === 'counselorJoin') mailOption.subject = '[라포 알림 서버] 새로운 상담사가 회원가입을 하였습니다.'
-      else if (type === 'reservation') mailOption.subject = '[라포 알림 서버] 새로운 예약 신청이 접수되었습니다.'
-      else reject(new CustomError('InternalServerError', 'Unintentional email notification type'));
+  const mailOption = {
+    from: "'라포 알림 서버' rapport5959@gmail.com",
+    to: 'rapport5959@naver.com',
+    html: '<p>라포 알림 서버에서 발송된 메일입니다.<p>'
+  }
+  if (type === 'counselorJoin') mailOption.subject = '[라포 알림 서버] 새로운 상담사가 회원가입을 하였습니다.'
+  else if (type === 'reservation') mailOption.subject = '[라포 알림 서버] 새로운 예약 신청이 접수되었습니다.'
 
-      resolve(mailOption);
-    } catch (error) {
-      reject(error);
-    }
-  })
+  return mailOption;
 }
 
 module.exports = mailOption;
